6e6ded43b5d9c5ad5b1e380cf5c0f69b4079ec75
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-21 11:36:17 +0000

    added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Johanna Liu All rights reserved
+// Copyright (c) 2022 Johanna Liu All rights reserved
 //
 // Created by: Johanna Liu
 // Created on: Mar 2022
@@ -13,20 +13,9 @@ if (navigator.serviceWorker) {
   })
 }
 
-;("use strict")
 /**
- * This function calculates area of a parrallelogram.
+ * This function displays an alert.
  */
-function calculate() {
-  // input
-  const length = parseInt(document.getElementById("length-of-pyramid").value)
-  const width = parseInt(document.getElementById("width-of-pyramid").value)
-  const height = parseInt(document.getElementById("height-of-pyramid").value)
-
-  // process
-  const volume = (length * width * height) / 3
-
-  // output
-  document.getElementById("answers").innerHTML =
-    "Volume is: " + volume.toFixed(2) + " cm³"
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
 }
