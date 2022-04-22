24085c6c41ea7bedcd5ab7bd5455ed86a66c9a5b
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-22 01:43:38 +0000

    add

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/, {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js, {
     scope: "/ICS2O-PWA-Test/",
   })
 }
